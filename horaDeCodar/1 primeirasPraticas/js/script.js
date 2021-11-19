05f99be050b8417b235ffe67b9e20d6a2d8f132a
Marcelo Santos <riquecelox462m@gmail.com>
2021-11-19 15:31:03 -0300

    Curso do Youtube javascript Hora de Codar

---
+++ horaDeCodar/1 primeirasPraticas/js/script.js
@@ -0,0 +1,1 @@
+console.log('Script no head')
---
+++ horaDeCodar/1.1 particularidades/js/scripts.js
@@ -0,0 +1,18 @@
+console.log('Primeiro');
+
+console.log('Último');
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a)
+
+//comentário de uma linha
+
+/* 
+comentário
+de 
+multiplas
+linhas
+*/
