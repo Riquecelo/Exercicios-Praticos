68e8e15aa9568d6f73080172b5781ccb2750d3ef
Marcelo Santos <riquecelox462m@gmail.com>
2021-11-19 21:34:59 -0300

    Add files via upload

---
+++ horaDeCodar/7_operadores_condicionais/script.js
@@ -0,0 +1,12 @@
+/*Sintaxe da estrutura condicional if */
+if (condition) {
+    
+}
+/**************************************/
+var idade = 20
+var idadeMinima = 18
+
+
+if (idade >= idadeMinima) {
+    console.log('Maior de idade')
+}
---
+++ horaDeCodar/7_operadores_condicionais/script2.js
@@ -0,0 +1,15 @@
+var nome = "Marcelo";
+
+if (nome == "Santos") {
+    console.log("O nome é: " + nome)
+}else{
+    console.log("O nome é outro")
+}
+
+if (nome == "Marcos") {
+    console.log("O nome é: " + nome)
+}else if (nome == "Marcelo") {
+    console.log(typeof nome)
+}else{
+    console.log("O nome é outro")
+}
