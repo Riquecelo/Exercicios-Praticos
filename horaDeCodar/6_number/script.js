5b48aa46d37231ff6cf1080e4a3310cd05959e10
Marcelo Santos <riquecelox462m@gmail.com>
2021-11-19 23:35:26 -0300

    Add files via upload

---
+++ horaDeCodar/6_number/script.js
@@ -0,0 +1,15 @@
+//números em javascript
+var numero = 5;
+ console.log(numero);
+ console.log(typeof numero); 
+
+ var float = 5.32;
+  console.log(float);
+  console.log(typeof float);
+
+//texto com números em javascript
+
+var textNumber='532';
+
+console.log(textNumber)
+console.log(typeof textNumber)
