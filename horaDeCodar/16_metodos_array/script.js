49aecb32ea5729c461cde836d1dd60e5762f354d
Marcelo Santos <riquecelox462m@gmail.com>
2021-11-22 16:05:21 -0300

    Add files via upload

--- horaDeCodar/16_metodos_array/script.js
+++ horaDeCodar/16_metodos_array/script.js
@@ -2,21 +2,21 @@
 
 //length/ verifica o comprimento do array
 var arr = [1, 2, 3, 4, 5]
-//console.log(arr.length)
+console.log(arr.length)
 
 //push/ adiciona elementos ao final do array
 arr.push(6)
 arr.push("algum")
-//console.log(arr)
-//console.log(arr.length)
+console.log(arr)
+console.log(arr.length)
 
 //pop/ remove elememtos do final do array
 arr.pop()
-//console.log(arr)
+console.log(arr)
 
 //unshift/ adiciona elementos ao início do array
 arr.unshift(0)
-//console.log(arr)
+console.log(arr)
 
 //shift/ retira elementos do início do array
 arr.shift()
@@ -27,3 +27,22 @@ console.log(arr[arr.length - 1])
 
 //isArray/ verifica se é array
 console.log(Array.isArray(arr))
+
+//splice/ adiciona elementos ao meio do array 
+arr.splice(2,0,999)
+console.log(arr)
+
+//splice/ remove elementos do array
+arr.splice(2,1)
+console.log(arr)
+
+//indexOf/ mostra o indice do elemento
+console.log(arr.indexOf(3))
+
+//join/ converte o array em string
+var arr2 = ['tenha', 'um', 'bom', 'dia']
+console.log(arr2.join(" "))
+console.log(arr2)
+
+//reverse/ inverte os elementos de um array
+console.log(arr2.reverse())
