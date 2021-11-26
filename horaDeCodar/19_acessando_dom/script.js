457c31a6214879fe7c49c59d59e2b315bca6f5b1
Marcelo Santos <riquecelox462m@gmail.com>
2021-11-25 22:11:22 -0300

    Add files via upload

---
+++ horaDeCodar/19_acessando_dom/script.js
@@ -0,0 +1,14 @@
+//tag
+var titulo = document.getElementsByTagName('h1')[0]
+console.log(titulo)
+
+var list = document.getElementsByTagName('li')
+console.log(list[3])
+
+//id
+var paragrafo = document.getElementById('paragrafo')
+console.log(paragrafo)
+
+//class
+var itemLista = document.getElementsByClassName('item')
+console.log(itemLista)
