7404627f34ca2065ffa265f9c11a69f929b992f0
Marcelo Santos <riquecelox462m@gmail.com>
2021-11-27 11:05:41 -0300

    Add files via upload

---
+++ horaDeCodar/23_removendoElemento/script.js
@@ -0,0 +1,13 @@
+//removendo elementos
+var subtitulo = document.querySelector('h2')
+
+subtitulo.remove()
+
+//removendo elemento filho
+
+var container = document.querySelector('#container')
+
+var p = document.querySelector('#container p')
+
+container.removeChild(p)
+
