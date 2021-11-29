0d21d286be9b828b88bc651a8446cab056cf4fcd
Marcelo Santos <riquecelox462m@gmail.com>
2021-11-29 18:15:33 -0300

    Add files via upload

---
+++ horaDeCodar/31_eventoOnload/script.js
@@ -0,0 +1,6 @@
+//Evento Onload - é ativado ao carregar a página depois realiza uma ação
+window.onload = function(){
+    console.log('Evento onload')
+}
+console.log('Carregou a página')
+console.log('Carregou o JS')
