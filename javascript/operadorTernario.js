c6674961b35cbf2c71fffa08381042a21e6ee812
Marcelo Santos <riquecelox462m@gmail.com>
2023-12-06 20:55:54 -0300

    Add files via upload

---
+++ javascript/operadorTernario.js
@@ -0,0 +1,6 @@
+//Opperador ternário
+var isTexto = false;
+var isNumero = false;
+var teste = false ? isTexto? 'é um texto':'não tem texto' : isNumero ? 'o numero é: ' + 10 : 'o numero é: '+ 0
+
+console.log(teste)
